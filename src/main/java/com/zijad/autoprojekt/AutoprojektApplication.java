e39e1609b24a9f215abc2e9ad1dd920fb054a979
yozi97 <lejliczijad@gmail.com>
2025-06-19 12:52:29 +0200

    Initial commit

---
+++ src/main/java/com/zijad/autoprojekt/AutoprojektApplication.java
@@ -0,0 +1,13 @@
+package com.zijad.autoprojekt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutoprojektApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutoprojektApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/zijad/autoprojekt/AutoprojektApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zijad.autoprojekt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutoprojektApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
